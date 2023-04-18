534bbcfa33ab84b5a34e8b73e734c16a6fb1f7b9
Kallum Burgin <Kallb123@users.noreply.github.com>
2023-04-18 23:05:14 +0100

    Render rectangle on user input

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,28 @@ import Map from './Components/Map';
 function App() {
   const [gridRef, setGridRef] = useState('NU 12765 42058');
   const [scale, setScale] = useState(1000);
+  const [width, setWidth] = useState(23.0);
+  const [height, setHeight] = useState(23.0);
   const [gridRefSet, setGridRefSet] = useState('NU 12765 42058');
   const [scaleSet, setScaleSet] = useState(1000);
+  const [widthSet, setWidthSet] = useState(23.0);
+  const [heightSet, setHeightSet] = useState(23.0);
 
   const updateVariables = () => {
     console.log("Updating variables");
     setGridRefSet(gridRef);
     setScaleSet(scale);
+    setWidthSet(width);
+    setHeightSet(height);
   }
 
   return (
     <div className="App">
-      <Map gridRef={gridRefSet} scale={scaleSet}></Map>
+      <Map gridRef={gridRefSet} scale={scaleSet} width={widthSet} height={heightSet}></Map>
       <label>Reference - <input type='text' value={gridRef} onInput={e => setGridRef(e.currentTarget.value)} /></label><br />
       <label>Scale - 1:<input type='number' value={scale} onInput={e => setScale(parseInt(e.currentTarget.value))} /></label><br />
+      <label>Width - <input type='number' value={width} onInput={e => setWidth(parseFloat(e.currentTarget.value))} /></label><br />
+      <label>Height - <input type='number' value={height} onInput={e => setHeight(parseFloat(e.currentTarget.value))} /></label><br />
       <button onClick={updateVariables}>Submit</button>
     </div>
   );
--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,22 +1,43 @@
-import { LatLngTuple } from 'leaflet';
+import { LatLngBoundsExpression, LatLngTuple } from 'leaflet';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import OsGridRef from '../gridref-to-latlng';
+
+import { Position, projectBearingDistance } from "aviation-math";
+import RectangleLayer from './RectangleLayer';
 
 interface Props {
   gridRef: string,
   scale: number,
+  width: number,
+  height: number,
 };
 
 const Map = (props: Props) => {
     const {
       gridRef,
-      scale
+      scale,
+      width,
+      height
     } = props;
 
     let parsedOS = OsGridRef.parse(gridRef);
     let convertedLatLng = OsGridRef.osGridToLatLong(parsedOS);
 
     const position : LatLngTuple = [convertedLatLng.lat, convertedLatLng.lon]
+
+    const widthMeters = scale * width / 100;
+    const heightMeters = scale * height / 100;
+    const diagonalDistance = Math.sqrt(widthMeters*widthMeters + heightMeters*heightMeters);
+    const diagonalDistanceNM = diagonalDistance / 1852;
+    // const topLeft = projectBearingDistance(new Position(position[0], position[1]), 315, diagonalDistanceNM/2);
+    const topRight = projectBearingDistance(new Position(position[0], position[1]), 45, diagonalDistanceNM/2);
+    // const bottomRight = projectBearingDistance(new Position(position[0], position[1]), 135, diagonalDistanceNM/2);
+    const bottomLeft = projectBearingDistance(new Position(position[0], position[1]), 225, diagonalDistanceNM/2);
+    
+    const rectangle : LatLngBoundsExpression = [
+      [bottomLeft.lat, bottomLeft.lon],
+      [topRight.lat, topRight.lon],
+    ]
 
     return (
       <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{height: "500px"}}>
@@ -29,6 +50,7 @@ const Map = (props: Props) => {
             A pretty CSS3 popup. <br /> Easily customizable. <span>{ gridRef }</span>
           </Popup>
         </Marker>
+        <RectangleLayer rectangle={rectangle} />
       </MapContainer>
     )
 
